8fd05bc5eb183c9d3d5ab50c16c195f63a74223b
StripedBear <55128637+StripedBear@users.noreply.github.com>
2025-09-12 15:43:27 +0800

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // оставим, если у вас нет image optimization
+    unoptimized: true, 
   },
 };
 
